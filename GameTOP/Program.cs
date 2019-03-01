d38177ac79395256683899d1c9f56dc08d1870cc
Alberto Stephanio Veloso <alberto.veloso@linx.com.br>
2019-03-01 12:14:14 -0300

    First commit

---
+++ GameTOP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GameTOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFoda("Anderson");
+            jogo.IniciarJogo();
+        }
+    }
+
+    class JogoFoda
+    {
+        public string _NomeJogador { get; }
+        public JogoFoda(string nome)
+        {
+            _NomeJogador = nome;
+        }
+        public void IniciarJogo()
+        {
+            Console.WriteLine($"{_NomeJogador} chutou a bola");
+        }
+        
+    }
+}
